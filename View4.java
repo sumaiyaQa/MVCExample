b224681fbe845a07e703081e8a14cf1b0b3ac826
Paula Vianca <168782647+pauvianca@users.noreply.github.com>
2025-02-21 21:35:54 +0400

    Update View4.java
    update of implementation for observer pattern

--- View4.java
+++ View4.java
@@ -1,50 +1,35 @@
 package mvcexample;
 
-// This is the pink view.
-// It displays the B component of the model (data base).
-// As supplied, this view does not subscribe to the model as an Observer,
-// and relies on its controller to call update to cause the view to be refreshed.
-
 import java.util.*;  // For Observer
 import java.awt.*;
 import javax.swing.*;
 
-public class View4 extends JPanel {
-  
+public class View4 extends JPanel implements Observer {
+
     private Model model;
-    private Controller2 contr;   // Parent Frame
-    
     private JTextField display;
     
-    // Constructor
-    public View4(Controller2 contr, Model model) {
-        
-        // Record references to the parent controller and the model
-        this.contr = contr;
+    public View4(Model model) {
         this.model = model;
+        
+        // Subscribe to the model
+        model.addObserver(this);
         
         // Set up view GUI
         setBackground(Color.pink);
         add(new JLabel("View4"));
         display = new JTextField("No data", 15);
         add(display);
-      
-    } // constructor
-    
-    // Called by controller to refresh the view:
-    public void update() {
-        
-        // Fetch (potentially) updated information and display it
+    }
+
+    // Update method required by Observer interface
+    @Override
+    public void update(Observable o, Object arg) {
         int b = model.getDataB();
         display.setText("Model data B: " + b);
-      
-    } // update
+    }
     
-    // Called by the controller to clear the view:
     public void clear() {
-        
         display.setText("");
-      
-    } // clear
-    
-} // class View4
+    }
+}
