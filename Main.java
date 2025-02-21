ae485ed19ff82da9cb2db22ddd391ba412cb8748
sumaiyaQa <168733927+sumaiyaQa@users.noreply.github.com>
2025-02-21 21:59:45 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,8 +11,8 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
+        Controller1 c1 = new Controller1(model, "Controller 1- Main Window",300,160); // Create first controller
+        Controller2 c2 = new Controller2(model, "Controller 2- Secondary Window",550,200); // Create second controller
         
     } // main
   
